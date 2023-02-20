3b85958b7ccf938c9057c17a47a81f043ae5f553
Renita Kurian <rrenita1206@gmail.com>
2023-02-20 20:12:07 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout <<"SRN: PES1UG20CS331\n"; 
+  cout <<"Creating a basic pipeline\n";
+  return 0;
+}
